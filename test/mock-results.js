fc3a735c570f17ec40a84553c2f92a04a28a410e
Yuliya Bagriy <aviskase@gmail.com>
2021-01-20 17:49:41 -0500

    Ensure that test time attribute is capped at 3 digits after the decimal

--- test/mock-results.js
+++ test/mock-results.js
@@ -10,7 +10,7 @@ module.exports = function(stats, options) {
           name: "Mocha Tests",
           tests: 4,
           failures: "2",
-          time: ((stats.duration || 0) / 1000).toFixed(4)
+          time: ((stats.duration || 0) / 1000).toFixed(3)
         }
       },
       {
@@ -21,7 +21,7 @@ module.exports = function(stats, options) {
               timestamp: "1970-01-01T00:00:00", // ISO timestamp truncated to the second
               tests: "0",
               failures: "0",
-              time: "0.0000"
+              time: "0.000"
             }
           }
         ]
@@ -34,7 +34,7 @@ module.exports = function(stats, options) {
               timestamp: "1970-01-01T00:00:00",
               tests: "3",
               failures: "2",
-              time: "100.0010"
+              time: "100.001"
             }
           },
           {
@@ -42,7 +42,7 @@ module.exports = function(stats, options) {
               _attr: {
                 name: "Foo Bar can weez the juice",
                 classname: "can weez the juice",
-                time: "0.1010"
+                time: "0.101"
               }
             }
           },
@@ -52,7 +52,7 @@ module.exports = function(stats, options) {
                 _attr: {
                   name: "Foo Bar can narfle the garthog",
                   classname: "can narfle the garthog",
-                  time: "2.0020"
+                  time: "2.002"
                 }
               },
               {
@@ -72,7 +72,7 @@ module.exports = function(stats, options) {
                 _attr: {
                   name: "Foo Bar can behave like a flandip",
                   classname: "can behave like a flandip",
-                  time: "30.0030"
+                  time: "30.003"
                 }
               },
               {
@@ -96,7 +96,7 @@ module.exports = function(stats, options) {
               timestamp: "1970-01-01T00:01:40", // new Date(100001).toISOString().slice(0, -5)
               tests: "1",
               failures: "0",
-              time: "400.0050"
+              time: "400.005"
             }
           },
           {
@@ -104,7 +104,7 @@ module.exports = function(stats, options) {
               _attr: {
                 name: "Another suite! works",
                 classname: "works",
-                time: "400.0040"
+                time: "400.004"
               }
             }
           }
@@ -148,7 +148,7 @@ module.exports = function(stats, options) {
             tests: "1",
             failures: "0",
             skipped: "1",
-            time: "0.0000"
+            time: "0.000"
           }
         },
         {
@@ -157,7 +157,7 @@ module.exports = function(stats, options) {
               _attr: {
                 name: "Pending suite! pending",
                 classname: "pending",
-                time: "0.0000"
+                time: "0.000"
               }
             },
             {
